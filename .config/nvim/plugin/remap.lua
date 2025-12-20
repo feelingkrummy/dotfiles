2259b14905c373b1f6e0c3ee9c9c922a74a97de8
feelingkrummy <kamrummel95@gmail.com>
2025-12-20 12:12:20 -0600

    Add some key remaps

--- .config/nvim/plugin/remap.lua
+++ .config/nvim/plugin/remap.lua
@@ -1,14 +1,19 @@
 local kr = require("kr")
 
+vim.keymap.set( 'n', '<leader>/', '<cmd>noh<CR>' )
+
+-- Navigation
 for index, letter in ipairs({ "h", "j", "k", "l" }) do
     vim.keymap.set( "n", "<C-".. letter .. ">", "<C-w>" .. letter )
 end
+vim.keymap.set('n', 'j', 'gj' ) -- May not need these anymore because no wrap
+vim.keymap.set('n', 'k', 'gk' )
 
-vim.keymap.set( 'n', 'j', 'gj' ) -- May not need these anymore because no wrap
-vim.keymap.set( 'n', 'k', 'gk' )
+vim.keymap.set('n', '<leader>n', ':tabN<CR>')
+vim.keymap.set('n', '<leader>m', ':tabn<CR>')
+vim.keymap.set('n', '<leader>N', ':$tabnew<CR>')
 
-vim.keymap.set( 'n', '<leader>/', '<cmd>noh<CR>' )
-
+-- Buffer Control
 vim.keymap.set('n', '<leader>q', function() kr.buffer_close() end)
 vim.keymap.set('n', '<leader>Q', function() kr.buffer_close{force = true} end)
 
